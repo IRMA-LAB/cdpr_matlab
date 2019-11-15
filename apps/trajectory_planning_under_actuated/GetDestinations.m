a29c3a2776958fc6357b1092f74217871b8faa8e
EdoardoIda <edoardo.ida@gmail.com>
2019-11-15 18:37:49 +0100

    frequency analysis initial work

--- apps/trajectory_planning_under_actuated/GetDestinations.m
+++ apps/trajectory_planning_under_actuated/GetDestinations.m
@@ -5,12 +5,16 @@ function data = GetDestinations(data,cdpr_p,record,ut)
     data.p(1:3,1)  = cdpr_p.workspace_center-[0.3;0;0.9];
     data.p(4:6,1)  = fsolve(@(v) FunGs(record,cdpr_p,data.p(1:3,1),v),...
     [0;0;0],ut.fsolve_options_grad);
+
+    [vector1,matrix1] = CalcNaturalFrequencies(cdpr_p,data.p(:,1));
     
     data.dt(1) = 2.5; 
     
     data.p(1:3,2)  = cdpr_p.workspace_center+[0.5;0;-0.9];
     data.p(4:6,2)  = fsolve(@(v) FunGs(record,cdpr_p,data.p(1:3,2),v),...
     [0;0;0],ut.fsolve_options_grad);
+
+    [vector2,matrix2] = CalcNaturalFrequencies(cdpr_p,data.p(:,2));
     
     data.dt(2) = 2; 
     
@@ -18,6 +22,7 @@ function data = GetDestinations(data,cdpr_p,record,ut)
     data.p(4:6,3)  = fsolve(@(v) FunGs(record,cdpr_p,data.p(1:3,3),v),...
     [0;0;0],ut.fsolve_options_grad);
     
+    [vector3,matrix3] = CalcNaturalFrequencies(cdpr_p,data.p(:,3));
     data.dt(3) = 2; 
     
      
