55744b0615ad84fedb44c14455f3d9b6c3f9572d
Simone Comari <simone.comari2@unibo.it>
2019-12-13 14:24:55 +0100

    more formatting

--- libs/under_actuated/CalcJacobianGs.m
+++ libs/under_actuated/CalcJacobianGs.m
@@ -9,7 +9,6 @@ for i = 1:n
   K = K + cdpr_v.tension_vector(i) .* [-T   T * a_tilde;...
     -a_tilde * T   (a_tilde * T - Anti(cdpr_v.cable(i).vers_t)) * a_tilde];
 end
-
 K(4:6, 4:6) = K(4:6, 4:6) + Anti(cdpr_v.platform.ext_load(1:3)) *...
   Anti(cdpr_v.platform.pos_PG_glob);
 matrix = cdpr_v.underactuated_platform.geometric_orthogonal' * K;
