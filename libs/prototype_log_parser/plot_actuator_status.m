c6befb879c32e263b83dc12703677841fed09ace
Simone Comari <simone.comari2@unibo.it>
2020-05-20 17:03:16 +0200

    added plotting utils for actuators msg data log

--- libs/prototype_log_parser/plot_actuator_status.m
+++ libs/prototype_log_parser/plot_actuator_status.m
@@ -9,15 +9,13 @@ data = parseCableRobotLogFile(filepath);
 
 actuators_id = unique(sorted_id);
 num_actuators = length(actuators_id);
-torques_mat = zeros(num_actuators, length(sorted_id)/num_actuators);
+torques_mat = zeros(num_actuators, ceil(length(sorted_id)/num_actuators));
 pulley_enc_mat = torques_mat;
-pulley_angle_mat = torques_mat;
 sorted_ts = torques_mat;
 for i = 1:num_actuators
     idx = sorting_idx(sorted_id == actuators_id(i));
     torques_mat(i, :) = data.actuator_status.values.motor_torque(idx);
     pulley_enc_mat(i, :) = data.actuator_status.values.aux_position(idx);
-    pulley_angle_mat(i, :) = data.actuator_status.values.pulley_angle(idx);
     sorted_ts(i, :) = data.actuator_status.timestamp(idx);
 end
     
@@ -31,23 +29,14 @@ for i = 1:num_actuators
     xlabel('[sec]')
     xlim([sorted_ts(i,1), sorted_ts(i,end)])
 end
+
 %% Plot pulley encoder values
-figure()
+figure('units','normalized','outerposition',[0 0 1 1])
 for i = 1:num_actuators
     subplot(num_actuators,1,i)
     plot(sorted_ts(i,:), pulley_enc_mat(i,:))
     grid on
     title(sprintf('Actuator #%d encoder values', actuators_id(i)))
-    xlabel('[sec]')
-    xlim([sorted_ts(i,1), sorted_ts(i,end)])
-end
-%% Plot pulley angles
-figure()
-for i = 1:num_actuators
-    subplot(num_actuators,1,i)
-    plot(sorted_ts(i,:), pulley_angle_mat(i,:))
-    grid on
-    title(sprintf('Actuator #%d pulley angles', actuators_id(i)))
     xlabel('[sec]')
     xlim([sorted_ts(i,1), sorted_ts(i,end)])
 end
